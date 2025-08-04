353aace0e30d525ee37b357e4261fbe1b5e637bd
navaxlab <navax,xyz.com>
2025-08-05 00:31:53 +0200

    Auto push

--- pushall.bat
+++ pushall.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "C:\Users\drjaz\OneDrive\Belgeler\Science\GitHub\Morpho Source auto\navax-licencing"
 git add .
-git commit -m "Auto update"
+git commit -m "Auto push"
 git push origin main
 pause
-
